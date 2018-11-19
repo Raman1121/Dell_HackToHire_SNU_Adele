d4bdd77aa819667aa15683649dc6c0819f3e82a6
Raman1121 <happy07.dutt@gmail.com>
2018-11-20 01:56:54 +0530

    Most updated at 2:00 am

--- Dell_arko/Dell/app/src/main/java/com/example/dell/Activity/Main3Activity.kt
+++ Dell_arko/Dell/app/src/main/java/com/example/dell/Activity/Main3Activity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import com.example.dell.R
 
 class Main3Activity : AppCompatActivity() {
@@ -13,10 +14,14 @@ class Main3Activity : AppCompatActivity() {
         setContentView(R.layout.activity_main_login)
 
         var btn_new_activity = findViewById(R.id.loginButton) as Button
+        var mEditTextEmail = findViewById<EditText>(R.id.edit_text_email) as EditText
+
+
 
         btn_new_activity.setOnClickListener {
             val intent = Intent(this@Main3Activity, MainActivity::class.java)
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            //intent.putExtra("Organization", organization)
             startActivity(intent)
         }
     }
--- Dell_arko/Dell/app/src/main/java/com/example/dell/Activity/MainActivity.java
+++ Dell_arko/Dell/app/src/main/java/com/example/dell/Activity/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -33,11 +34,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         //overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -72,8 +75,22 @@ public class MainActivity extends AppCompatActivity
 
         ArrayList<Items> itemsArrayList = new ArrayList<>();
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        itemsArrayList.add(new Items("Laptop1","very good laptop","100" ,"5", R.drawable.dell));
-        itemsArrayList.add(new Items("Laptop2","Very bad","200rs","3", R.drawable.dell));
+        itemsArrayList.add(new Items("LG Electronics G Watch", "LG G Smart Watch for select Android devices. Bluetooth 4.0 technology lets you easily sync with your compatible Android device to this LG G Watch, so you can receive and view important mobile notifications"
+                , "110.99","3.8", R.drawable.wearable1));
+        itemsArrayList.add(new Items("Fossil Q Wander Silicone Touchscreen Smartwatch", "Q Wander is our newest digital display watch that connects seamlessly to your phone."
+                , "255","4.5", R.drawable.wearable2));
+        itemsArrayList.add(new Items("Apple Watch Nike", "Like all Apple Watch Series 2 models, Apple Watch Nike+ has built-in GPS to track your pace, distance, and route — even if you don’t have your iPhone with you"
+                , "439.99","5", R.drawable.wearable3));
+        itemsArrayList.add(new Items("SAMSUNG Gear Fit2 SM-R360", "When you’re looking for motivation to stay at your best, the Samsung Gear Fit2 keeps you going. It automatically recognizes whatever you’re doing—from walking up a flight of stairs to working out at the gym"
+                , "119.99","4", R.drawable.wearable4));
+        itemsArrayList.add(new Items("Motorola Moto 360", "The Motorola Moto 360 Smartwatch comes with a 22mm black leather strap, runs on the Android Wear operating system and connects to your Android smartphone via Bluetooth to give you the information you need"
+                , "99.99","3.5", R.drawable.wearable5));
+        itemsArrayList.add(new Items("LONSUN TW64", "LONSUN TW64 Smart Watch TopBest Bluetooth Bracelet Call Remind Remote self-timer Smart band Calorie Counter Wireless Pedometer Sport Sleep Monitor Activity Tracker For Android IOS Phone (Black)"
+                , "13.99","2", R.drawable.wearable6));
+        itemsArrayList.add(new Items("LEMFO LEM5", "Frequency Band: 2G: GSM 850 900 1900 2100/ 3G: WCDMA 850 1900 2100 SIM: NANO SIM Operating system: Android 5.1 RAM: 1GB ROM: 8GB CPU: MTK6580 CPU SPEED: 1.3G Quad core SCREEN SIZE: 1.39\" IPS OLED Touc"
+                , "129.99","3.8", R.drawable.wearable7));
+        itemsArrayList.add(new Items("Luxsure Uwatch", "Luxsure Uwatch Smart Watch with Heart Rate Monitor Android Smart Watch Phone Sports Bluetooth Wristwatch With 3G magsensor gravity sensor Compatible With IOS & Android (Silver)"
+                , "68.99","3.4", R.drawable.wearable8));
         ItemsAdapter itemsAdapter = new ItemsAdapter(MainActivity.this, itemsArrayList, false);
         GridLayoutManager manager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(manager);
@@ -128,11 +145,18 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.analytics) {
+            openAnalytics();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openAnalytics(){
+        Intent i = new Intent(MainActivity.this, analytics.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
